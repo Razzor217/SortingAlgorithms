b68d06b529a15b6cd0327c6957796cd0b3c09d22
Max Beddies <ubyjy@student.kit.edu>
2018-07-07 18:30:58 +0200

    Added semi recursive quick sort

--- src/main/Main.java
+++ src/main/Main.java
@@ -13,6 +13,7 @@ import sorting.LSDRadixSort;
 import sorting.MergeSort_AL;
 import sorting.MergeSort_LL;
 import sorting.QuickSort;
+import sorting.QuickSort_SemiRec;
 
 public class Main {
 
@@ -21,8 +22,8 @@ public class Main {
     //       Or even just subclasses of algorithm.
 	public static void main(String[] args) {
 		
-	    int n = 5000000;
-	    int range = 1000;
+	    int n = 5000;
+	    int range = 100;
 		
 		int[] a = new int[n];
 		List<Integer> list = new LinkedList<Integer>();
@@ -34,13 +35,15 @@ public class Main {
 		}
 		
 		Algorithm[] b = {
-	        //new InsertionSort(a), 
-	        //new MergeSort_AL(list),
-	        //new QuickSort<Integer>(a)//,
-	        //new MergeSort_LL(list),
-	        //new BeadSort(a),
-	        //new BeadSort_Horizontal(a),
-		    //new HeapSort_Int(a)
+	        new InsertionSort(a), 
+	        new MergeSort_AL(list),
+	        new QuickSort(a),
+	        new MergeSort_LL(list),
+	        new BeadSort(a),
+	        new BeadSort_Horizontal(a),
+		    new HeapSort_Int(a),
+		    new LSDRadixSort(a, 10, (int) Math.log10(range) + 1),
+		    new QuickSort_SemiRec(a, 10)
 	        };
 		
 		System.out.println("Sum of elements in array: " 
@@ -49,15 +52,16 @@ public class Main {
 	        + String.format("%33s", "[" + 1 + "," + range + "]"));
 		System.out.println("Algorithms in ascending order of");
 		System.out.println("execution time:");
-		System.out.println("----------------------------------------");
+		System.out.println("-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*");
 		//printAlgorithms(b);
 		
 		
 		int[] test = new int[] {9, 8, 7, 6, 5, 4, 3, 2, 1, 0};
 		int[] test2 = new int[] {9, 5, 6, 3, 4, 1, 2, 0, 11, 9};
-		System.out.println(Arrays.toString(test));
-		LSDRadixSort sort = new LSDRadixSort(test, 10, 2);
-		System.out.println(Arrays.toString(sort.lsdRadixSort(test)));
+		System.out.println(Arrays.toString(test2));
+		QuickSort_SemiRec.quickSort(test2, 0, 9, 3);
+		System.out.println(Arrays.toString(test2));
+		
 		
 	}
 	
---
+++ src/sorting/QuickSort_SemiRec.java
@@ -0,0 +1,56 @@
+package sorting;
+
+/**
+ * Implements a semi recursive variant of quick sort which uses
+ * insertion sort once the list is small enough.
+ * Additionally, recursion only gets applied on the smaller part of the list.
+ * 
+ * @author Max Beddies
+ *
+ */
+public class QuickSort_SemiRec extends QuickSort {
+
+    private int n;
+    
+    /**
+     * Initializes a semi recursive quick sort with an integer array and 
+     * an integer determining when to use insertion sort instead of quick sort
+     * @param a The unsorted array
+     * @param n List length marking the switch to insertion sort
+     */
+    public QuickSort_SemiRec(int[] a, int n) {
+        super(a);
+        this.n = n;
+        setName("QuickSort (semi recursive)");
+    }
+    
+    /**
+     * Executes a semi recursive quick sort as specified in the class 
+     * description
+     * @param a The unsorted integer array
+     * @param l Index of the left-most element
+     * @param r Index of the right-most element
+     * @param n Length marking the switch to insertion sort
+     * @return The sorted array
+     */
+    public static int[] quickSort(int[] a, int l, int r, int n) {
+        if (l<r) {
+            while (r-l+1 > n) {
+                int k = pickPivot(a, l, r);
+                int m = partition(a, l, r, k);
+                int floor = (int) Math.floor((l+r)/2d);
+                // Use quick sort recursively only on smaller part of the array
+                if (m<floor) {
+                    quickSort(a, l, m-1, n);
+                    l = m+1;
+                } else {
+                    quickSort(a, m+1, r);
+                    r = m-1;
+                }
+            }
+            InsertionSort.insertionSortV2(a, l, r);
+        }
+        return a;
+    }
+
+}
