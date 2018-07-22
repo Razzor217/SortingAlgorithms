8224625f7503d35d649c7a0d65a0a917821a0975
Max Beddies <ubyjy@student.kit.edu>
2018-07-22 02:11:00 +0200

    Fixed mistake in InsertionSort.c

--- InsertionSort.h
+++ InsertionSort.h
@@ -7,10 +7,8 @@
  * @param a Unsorted array of integers
  * @param l Index of the left-most element
  * @param r Index of the right-most element
- * @param n Length of the array
  * @return 0 if the sorting was successfull, 1 if errors occured
  */
-//int insertionSort(int*, int, int, int);
-int insertionSort(int* a, int l, int r, int n);
+int insertionSort(int* a, int l, int r);
 
 #endif
--- Main.c
+++ Main.c
@@ -7,7 +7,7 @@ void print(int*, int n);
 
 int main() {
 
-	int n = 10;
+	int n = 20;
 	int* a = calloc(n, sizeof(int));
 	int i;
 	// Create sample data
@@ -15,21 +15,24 @@ int main() {
 		a[i] = n-i-1;
 
 	print(a, n);
-
-	if (insertionSort(a, 0, n-1, n))
-		return 1;
-
+	if (insertionSort(a, 0, n-1))
+		return EXIT_FAILURE;
 	print(a, n);
 
 	free(a);
 	return 0;
 }
 
+/* 
+ * Prints an array of integers with given length.
+ * @param a The integer array
+ * @param n The length of the array (count of elements)
+ */
 void print(int* a, int n) {
 	int i;
 	for (i=0; i<n; i++)
 		if (i<n-1)
-			printf("%i, ", a[i]);
+			printf("%i, ", *(a+i));
 		else
-			printf("%i\n", a[i]);
+			printf("%i\n", *(a+i));
 }
