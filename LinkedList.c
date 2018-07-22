268b236b380f8d0b6bd934e2b02bdfc685724cd4
Max Beddies <ubyjy@student.kit.edu>
2018-07-22 03:34:36 +0200

    Added first, last, isEmpty, pushFront, pushBack

--- LinkedList.c
+++ LinkedList.c
@@ -5,3 +5,46 @@ struct node {
 	Node* prev;
 	Node* next;
 };
+
+int first(Node* head) {
+	if (!isEmpty(head))
+		return head->value;
+	return -1;
+}
+
+int last(Node* head) {
+	if (!isEmpty(head))
+		return head->prev->value;
+	return -1;
+}
+
+int isEmpty(Node* head) {
+	if (head == NULL)
+		return 1;
+	return 0;
+}
+
+void pushFront(Node* head, int a) {
+	Node* node = malloc(sizeof(Node));
+
+	node->value = a;
+	node->prev = head->prev;
+	node->next = head;
+
+	head = node;
+}
+
+void pushBack(Node* head, int a) {
+	Node* node = malloc(sizeof(Node));
+
+	node->value = a;
+	node->prev = head->prev;
+	node->next = NULL;
+
+	head->prev = node;
+}
+
+int popFront(Node* head) {
+	//TODO
+	return 1;
+}
--- LinkedList.h
+++ LinkedList.h
@@ -1,47 +1,61 @@
 #ifndef LinkedList_h
 #define LinkedList_h
 
+#include <stddef.h>
+#include <stdlib.h>
+
+/*
+ * A simple list node, containing an integer value, 
+ * a pointer to the previous node and a pointer to the next node in the list.
+ */
 typedef struct node Node;
 
 /*
  * Returns the value of the first element in the list.
- * Returns NULL if the list is empty.
+ * 		head 		- Node representing the head of the list, a dummy-element
+ * 		Returns 	- (-1) if the list is empty
  */
-int first(void);
+int first(Node* head);
 
 /*
  * Returns the value of the last element in the list.
- * Returns NULL if the list is empty.
+ *		head 		- Node representing the head of the list
+ * 		Returns 	- (-1) if the list is empty
  */
-int last(void);
+int last(Node* head);
 
 /*
  * Returns 1 if the list is empty, 0 if it is not.
+ * 		head 		- Node representing the head of the list
  */
-int isEmpty(void);
+int isEmpty(Node* head);
 
 /*
  * Inserts an integer at the front of the list.
- * Returns 1 on success, 0 on failure.
+ *		head 		- Node representing the head of the list
+ *		a 			- Integer to be inserted at the front of the list
  */
-int pushFront(int);
+void pushFront(Node* head, int a);
 
 /*
  * Inserts an integer at the back of the list.
- * Returns 1 on success, 0 on failure.
+ *		head 		- Node representing the head of the list
+ *		a 			- Integer to be inserted at the back of the list
  */
-int pushBack(int);
+void pushBack(Node* head, int a);
 
 /*
  * Retrieves and removes an integer at the front of the list.
- * Returns NULL if the list is empty.
+ *		head 		- Node representing the head of the list
+ * 		Returns		- (-1) if the list is empty.
  */
-int popFront(void);
+int popFront(Node* head);
 
 /*
  * Retrieves and removes an integer at the back of the list.
- * Returns NULL if the list is empty.
+ *		head 		- Node representing the head of the list
+ * 		Returns 	- (-1) if the list is empty.
  */
-int popBack(void);
+int popBack(Node* head);
 
 #endif
